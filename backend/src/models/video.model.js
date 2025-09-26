d2c2e1f398af0a04f342984548b786645db7ca22
Vikash Kumar <vikash01.nk@gmail.com>
2025-09-27 05:02:27 +0530

    video.model.js setup

---
+++ backend/src/models/video.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema({
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        reg: "Post",
+        required: true
+    },
+    videoFile: {
+        type: String,
+        required: true
+    },
+    publicId: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const Video = mongoose.model("Video", videoSchema)
