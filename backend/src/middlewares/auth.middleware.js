2f63e173311883a36a7f419d5ff05bfa34f3d9ee
Vikash Kumar <vikash01.nk@gmail.com>
2025-10-04 16:15:34 +0530

    fix some minute bugs in auth middleware

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ import { User } from "../models/user.model.js";
 const verifyJWT = async (req, res, next) => {
 
     try {
-        const token = req.cookeis.accessToken || req.header();
+        const token = req.cookeis?.accessToken || req.header("Authorization").replace("Bearer ", "")
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
             if (decoded) {
@@ -19,13 +19,13 @@ const verifyJWT = async (req, res, next) => {
         const user = await User.findById(decodedToken._id).select("-password -refreshToken")
 
         if (!user) {
-            throw new ApiError(404, "ERROR: invalid access token");
+            throw new ApiError(401, "ERROR: invalid access token");
         }
 
         req.user = user;
         next();
 
     } catch (error) {
-        throw new ApiError(404, "ACCESS_TOKEN_ERROR: invalid access token while authorization");
+        throw new ApiError(401, "ACCESS_TOKEN_ERROR: invalid access token while authorization");
     }
 }
