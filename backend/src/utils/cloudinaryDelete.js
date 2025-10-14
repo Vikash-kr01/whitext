ca4f3ab2611163992496de084477b0a39d2e6669
Vikash Kumar <vikash01.nk@gmail.com>
2025-10-14 18:32:26 +0530

    add some comment

--- backend/src/utils/cloudinaryDelete.js
+++ backend/src/utils/cloudinaryDelete.js
@@ -17,6 +17,7 @@ const cloudinaryDelete = async (publicIds = []) => {
 
     try {
         const deletedImages = await cloudinary.api.delete_resources(publicIds, { resource_type: "image" });
+        // console.log(deletedImages)  // see bottom a>
         const deletedVideos = await cloudinary.api.delete_resources(publicIds, { resource_type: "video" });
 
         return {
@@ -29,3 +30,18 @@ const cloudinaryDelete = async (publicIds = []) => {
 }
 
 export { cloudinaryDelete };
+
+
+
+
+
+/*  a>  console.log(deletedImages) 
+    {
+        deleted: { npbdd5ofwjbnuabxe7ze: 'deleted' },
+        deleted_counts: { npbdd5ofwjbnuabxe7ze: { original: 1, derived: 0 } },
+        partial: false,
+        rate_limit_allowed: 500,
+        rate_limit_reset_at: 2025-10-14T13:00:00.000Z,
+        rate_limit_remaining: 497
+    }
+*/
