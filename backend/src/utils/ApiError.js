2a99b8d4a3d385a221212bac97b785bcb9fd7faa
Vikash Kumar <vikash01.nk@gmail.com>
2026-06-02 12:58:08 +0530

    fix ApiError class add stack handle

--- backend/src/utils/ApiError.js
+++ backend/src/utils/ApiError.js
@@ -11,6 +11,12 @@ class ApiError extends Error {
         this.errors = errors,
         this.success = false,
         this.data = null
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
     }
 }
 
