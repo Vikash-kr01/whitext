169fa7e1969a2d859e1bf5d293a616b0450c8d1d
Vikash Kumar <vikash01.nk@gmail.com>
2026-06-08 14:44:20 +0530

    add react-router and some random work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import "../src/style/App.css"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Working</h1>
+      <h1>Hello Yrrr...</h1>
     </>
   )
 }
