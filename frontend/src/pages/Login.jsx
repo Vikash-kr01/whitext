60e4ec631f4fb682661595b4a44dccf89924f23e
Vikash Kumar <vikash01.nk@gmail.com>
2026-06-10 06:15:58 +0530

    setup of login page frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../style/Login.css"
+
+import { Link } from 'react-router'
+import { useForm } from "react-hook-form"
+
+
+
 
 const Login = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, isSubmitting },
+    // formState link: "https://www.react-hook-form.com/api/useform/formstate/"
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const delay = async (d) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve()
+      }, d * 1000);
+    })
+  }
+
+  const onSubmit = async (data) => {
+    await delay(2)
+    console.log(data)
+    return 
+  }
+
+
   return (
-    <div>
-      This is login page
+    <div className='login'>
+      <div className='form'>
+        <h4>Login to whitext</h4>
+        <form onSubmit={handleSubmit(onSubmit)} action="/login">
+          <input
+            type="text"
+            placeholder="Email, username or mobile number"
+            {...register("email", {
+              required: true,
+              minLength: 1,
+            })}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            {...register("password", {
+              required: true,
+              minLength: 3,
+            })}
+          />
+
+          <input type="submit" disabled={!isValid || isSubmitting} value={isSubmitting ? "Logging In" : "Login"} className='submit'  />
+          {/* disable the submit btn if the form is not valid or is submitting */}
+          
+
+        </form>
+
+        <Link>Forgot password?</Link>
+      </div>
     </div>
   )
 }
