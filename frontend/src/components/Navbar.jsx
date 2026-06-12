2a19ef4d8c4cb0570c9d250797917c9ab126ac03
Vikash Kumar <vikash01.nk@gmail.com>
2026-06-12 16:44:44 +0530

    add css to navbar to slide up and down while scrolling main page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import { NavLink } from 'react-router'
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router";
+import "../style/Navbar.css";
 
 const Navbar = () => {
-	return (
-		<nav>
-			<NavLink to={"/home"} >Home</NavLink>
-			<NavLink to={"/notifications"} >Notification</NavLink>
-			<NavLink to={"/profile"} >Profile</NavLink>
-		</nav>
-	)
-}
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-export default Navbar
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY) {
+        // Scrolling down
+        setShowNavbar(false);
+      } else {
+        // Scrolling up
+        setShowNavbar(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <nav className={`navbar ${showNavbar ? "show" : "hide"}`}>
+      <NavLink to={"/home"}>Home</NavLink>
+      <NavLink to={"/notifications"}>Notification</NavLink>
+      <NavLink to={"/profile"}>Profile</NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
