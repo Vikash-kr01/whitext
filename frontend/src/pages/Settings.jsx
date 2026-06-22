6ec6dd79a0f6ec04770d15a2f63995c5ea04c5ac
Vikash Kumar <vikash01.nk@gmail.com>
2026-06-22 22:56:36 +0530

    work on Setting.jsx

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import "../style/Setting.css";
+
+import { Link } from "react-router";
+
+import { GrNext } from "react-icons/gr";
 
 const Settings = () => {
-  return (
-    <div>
-      This is Setting Page
-    </div>
-  )
-}
 
-export default Settings
+
+  return (
+    <div className="main-body">
+      {" "}
+      {/* miscellaneous.css */}
+      <div className="setting-container">
+        <Link className="setting-link">
+          <div className="setting-box">
+            <div className="setting-link-name">Change Password</div>
+            <GrNext />
+          </div>
+        </Link>
+        <Link className="setting-link">
+          <div className="setting-box">
+            <div className="setting-link-name">Delete Account</div>
+            <GrNext />
+          </div>
+        </Link>
+        <Link className="setting-link">
+          <div className="setting-box">
+            <div className="setting-link-name">Change Email</div>
+            <GrNext />
+          </div>
+        </Link>
+        <Link className="setting-link">
+          <div className="setting-box">
+            <div className="setting-link-name">Change Username</div>
+            <GrNext />
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
