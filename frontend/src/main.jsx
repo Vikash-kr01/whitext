7f2037307ba590c1125cb0a4752ff53e3da2faef
Vikash Kumar <vikash01.nk@gmail.com>
2026-06-22 22:56:01 +0530

    change the name of file miscellaneous.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import "./style/miscellanious.css"
+import "./style/miscellaneous.css"
 import { RouterProvider } from 'react-router'
 import router from './router.jsx'
 
