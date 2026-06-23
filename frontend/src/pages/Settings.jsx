6481b7e2d06286228297443eeb759e36d9d7570d
Vikash Kumar <vikash01.nk@gmail.com>
2026-06-23 16:35:16 +0530

    a little work on right page setting.jsx

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -12,7 +12,7 @@ const Settings = () => {
     <div className="main-body">
       {" "}
       {/* miscellaneous.css */}
-      <div className="setting-container">
+      <div className="setting-mid-page">
         <Link className="setting-link">
           <div className="setting-box">
             <div className="setting-link-name">Change Password</div>
@@ -37,6 +37,9 @@ const Settings = () => {
             <GrNext />
           </div>
         </Link>
+      </div>
+      <div className="setting-right-page">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odio neque enim expedita optio, fugiat modi beatae cum ab voluptate. Porro hic reiciendis velit maiores voluptates vero ut quia quasi ullam, vel ex adipisci quisquam provident optio harum illo repellendus inventore ad doloribus earum at commodi beatae magni. Repellendus voluptate rerum vel qui. Hic repudiandae obcaecati, non molestiae, quasi, ipsa tempore doloribus quidem esse dolorem adipisci nesciunt nam earum autem sapiente vitae possimus iusto quae cum? Officia natus sunt blanditiis, quam molestias ad placeat quos. Saepe voluptatibus dignissimos eligendi quod repudiandae deleniti corporis cum cupiditate ad! Sequi incidunt ullam quas voluptatem praesentium asperiores. Voluptas laboriosam repellendus dolorem commodi modi, quis ipsum placeat, iste pariatur ad, nihil molestias amet! Quas ea aliquam dolor molestias.
       </div>
     </div>
   );
