6a70fc19f255896e5e03fa1c39e5981074ab7d24
Vikash Kumar <vikash01.nk@gmail.com>
2026-07-20 13:12:52 +0530

    add proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {       
+      "/app": "http://127.0.0.1:4000"
+    }
+  },
   plugins: [react()],
 })
